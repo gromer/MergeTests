a45e27ebd207ff87d0d72d59fdd5eb1297976acd
Mike Gromer <mgromer@macu.com>
2013-04-02 21:52:53 -0600

    Adding best class ever.

---
+++ Texas.cs
@@ -0,0 +1,29 @@
+namespace Merge.Tests.Now.Thanks
+{
+	public class Texas
+	{
+		#region Properties
+		
+		public bool IsAwesome
+		{
+			get { return true; }
+		}
+		
+		#endregion Properties
+		
+		#region Random Methods
+		
+		public int getRandomNumber()
+		{
+			return 4; 	// chosen by fair dice roll.
+						// guaranteed to be random.
+		}
+		
+		#endregion Random Methods
+		
+		public void Main(string[] arrrrrrrgs)
+		{
+			Console.WriteLine("LOL I'M A PIRATE!");
+		}
+	}
+}
